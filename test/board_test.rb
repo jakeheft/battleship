13e16fdbec03ad436e76b1c0345f703cab1e0c81
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-25 11:53:48 -0600

    Change refutes to assert_equal false

--- test/board_test.rb
+++ test/board_test.rb
@@ -40,8 +40,8 @@ class BoardTest < MiniTest::Test
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
-    refute board.valid_placement?(cruiser, ["A1", "A2"])
-    refute board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false,  board.valid_placement?(submarine, ["A2", "A3", "A4"])
   end
 
   def test_coordinates_are_consecutive
@@ -49,10 +49,10 @@ class BoardTest < MiniTest::Test
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
-    refute board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    refute board.valid_placement?(submarine, ["A1", "C1"])
-    refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    refute board.valid_placement?(submarine, ["C1", "B1"])
+    assert_equal false,  board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    assert_equal false,  board.valid_placement?(submarine, ["A1", "C1"])
+    assert_equal false,  board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal false,  board.valid_placement?(submarine, ["C1", "B1"])
   end
 
   def test_coordinates_cant_be_diagonal
@@ -60,8 +60,8 @@ class BoardTest < MiniTest::Test
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
-    refute board.valid_placement?(cruiser, ["A1", "B2", "C3"])
-    refute board.valid_placement?(submarine, ["C2", "D3"])
+    assert_equal false,  board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    assert_equal false,  board.valid_placement?(submarine, ["C2", "D3"])
   end
 
 end

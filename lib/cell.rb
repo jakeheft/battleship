36d4758ae296daaaeb274e12878f4ccfc466064e
Jake Heft <jakeheft@gmail.com>
2020-08-24 21:42:02 -0600

    Add tests for rendering

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,6 +4,8 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_status = false  ## should @fired_upon be cell status? If so, write an if
+      ## statement for fired_upon? method
   end
 
   def empty?
@@ -11,6 +13,35 @@ class Cell
   end
 
   def place_ship(ship)
-    @ship = ship    
+    @ship = ship
+  end
+
+  def fired_upon?
+    @fired_status
+  end
+
+  def fire_upon
+    @fired_status = true
+    if empty? == false
+      @ship.hit
+    end
+  end
+
+  def render(reveal = false)
+    if reveal == false
+      if @fired_status == false
+        "."
+      elsif @fired_status == true && @ship == nil
+        "M"
+      end
+    elsif reveal == true
+      if @ship.sunk?
+        "X"
+      elsif @ship != nil && @fired_status == false
+        "S"
+      elsif @ship != nil && @fired_status == true
+        "H"
+      end
+    end
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -32,4 +32,34 @@ class CellTest < Minitest::Test
     assert_equal cruiser, cell.ship
     cell.empty?
   end
+
+  def test_cell_can_be_fired_upon
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+    assert_equal false, cell.fired_upon?
+    cell.fire_upon
+    assert_equal 2, cell.ship.health
+    assert cell.fired_upon?
+  end
+
+  def test_cell_can_render_status
+    cell_1 = Cell.new("B4")  ## convention ?? cell_1 vs cell1
+
+    assert_equal ".", cell_1.render
+    cell_1.fire_upon
+    assert_equal "M", cell_1.render
+  end
+
+  def test_cell_can_run_true_render
+    cell_1 = Cell.new("B4")
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+  end
+
 end

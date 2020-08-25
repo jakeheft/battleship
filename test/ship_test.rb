ad90d62e3059f487ddf451009a23f2c40440ac78
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-24 19:20:51 -0600

    Change ship to sunk? is true when no health left

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -39,4 +39,19 @@ class ShipTest < MiniTest::Test
     assert_equal 1, cruiser.health
   end
 
+  def test_ship_is_sunk_when_no_health_remaining
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 3, cruiser.health
+    cruiser.hit
+    assert_equal 2, cruiser.health
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    assert_equal 1, cruiser.health
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    assert_equal 0, cruiser.health
+    assert_equal true, cruiser.sunk?
+  end
+
 end

79417fb290d771a22452437deb5f04cd22b560a2
Jake Heft <jakeheft@gmail.com>
2020-08-27 14:03:44 -0600

    Create game class and test it exists

---
+++ test/game_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+class GameTest < MiniTest::Test
+
+  def test_it_exists
+    game = Game.new
+
+    assert_instance_of Game, game
+  end
+end

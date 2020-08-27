5caf64c87949f32199e0794f1b3cda842b513d6e
Jake Heft <jakeheft@gmail.com>
2020-08-27 17:19:24 -0600

    Create computer class, test it exists

--- lib/game.rb
+++ lib/game.rb
@@ -14,20 +14,12 @@ class Game
 
 #run coordinates = gets.chomp in game start method, then coordinates are carried into player.place_ship method via argument
   def place_ship(ship, coordinates_string)
-    # player.query_place_ship
-    # computer.query_place_ship
-    # coordinates = coordinates_string.split(" ")
+    coordinates = coordinates_string.split(" ")
 
     coordinates.each do |coordinate|
       @board.cells[coordinate].place_ship(ship)
       # require "pry"; binding.pry
     end
-  end
-
-  def fire_upon(cell)
-    # player.query_fire_upon
-    # computer.query_fire_upon
-    cell.fire_upon
   end
 
   def start
---
+++ test/computer_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/computer'
+
+class ComputerTest < Minitest::Test
+  def test_it_exists
+    computer = Computer.new
+
+    assert_instance_of Computer, computer
+  end
+
+end
